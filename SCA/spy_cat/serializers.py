1c4a2aef3265921bb7a7c0a95ad50782c0531cbe
xKuroiUsagix <povoroznikvitalii@gmail.com>
2024-10-31 16:03:09 +0200

    fix cat output by providing id in response

--- SCA/spy_cat/serializers.py
+++ SCA/spy_cat/serializers.py
@@ -6,4 +6,5 @@ from .models import SpyCat
 class SpyCatSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpyCat
-        fields = ['name', 'experience', 'breed', 'salary']
+        fields = ['id', 'name', 'experience', 'breed', 'salary']
+        read_only_fields = ['id']
